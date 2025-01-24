d002d92d0cc50c55f7d690d1163ae9a103c492b0
Renato Ferreira <renatoferreira.dev@gmail.com>
2025-01-24 12:40:18 +0100

    feat: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["randomuser.me"],
+    formats: ["image/avif", "image/webp"],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
