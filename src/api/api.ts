d6c9e4e7a70a14ea9928ef03176eebf26a032e46
Renato Ferreira <renatoferreira.dev@gmail.com>
2025-01-28 21:54:39 +0100

    feat: create function to load schedules dates

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,50 +1,27 @@
 import dayjs from "dayjs";
-import { CustomEventProps, Statuses } from "../@types/schedule";
+import { CustomEventProps } from "@/types/schedule";
+import { generateSchedules } from "./generate-random-data";
 
-const schedules: CustomEventProps[] = [
-  {
-    name: "Sandra Black",
-    title: "Hair cut & color",
-    status: Statuses.COMPLETED,
-    occupation: "Hair cut & makeup",
-    end: new Date(2025, 0, 23, 10, 30),
-    start: new Date(2025, 0, 23, 9, 30),
-    avatar: "https://randomuser.me/api/portraits/men/2.jpg",
-  },
-  {
-    name: "Miguel Peterson",
-    title: "Nail cut & paint",
-    status: Statuses.CANCELED,
-    occupation: "Hair cut & makeup",
-    start: new Date(2025, 0, 23, 10, 30),
-    end: new Date(2025, 0, 23, 11, 0),
-    avatar: "https://randomuser.me/api/portraits/men/2.jpg",
-  },
-  {
-    name: "Janet Smith",
-    title: "Skin cleaning",
-    status: Statuses.BOOKED,
-    occupation: "Hair cut & makeup",
-    start: new Date(2025, 0, 23, 12, 0),
-    end: new Date(2025, 0, 23, 13, 0),
-    avatar: "https://randomuser.me/api/portraits/men/2.jpg",
-  },
-];
+const randomSchedules = generateSchedules();
 
 export function getSchedulesByDate(date: Date): Promise<CustomEventProps[]> {
-  const promise = new Promise<CustomEventProps[]>((accept, reject) => {
+  const promise = new Promise<CustomEventProps[]>((accept) => {
     setTimeout(() => {
-      try {
-        accept(
-          schedules.filter((schedule) =>
-            dayjs(schedule.start).isSame(date, "D")
-          )
-        );
-      } catch {
-        reject({
-          error: { message: "Error while getting the schedules data" },
-        });
-      }
+      accept(
+        randomSchedules.filter((schedule) =>
+          dayjs(schedule.start).isSame(date, "D")
+        )
+      );
+    }, 1000);
+  });
+
+  return promise;
+}
+
+export function getSchedulesDates(): Promise<Date[]> {
+  const promise = new Promise<Date[]>((accept) => {
+    setTimeout(() => {
+      accept(randomSchedules.map((schedule) => schedule.start));
     }, 1000);
   });
 
